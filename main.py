eda7ab62f682fd328fedddc2047f6303940f306f
pete <pete@syncad.com>
2017-06-02 16:35:47 -0400

    3rd grader
    Are you smarter than a 3rd grader?

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ for i in range(10):
     print i
 print 'bye'
 print 'something else'
+#
+print 'now for some fun'
+a = raw_input('Are you smarter than a 3rd grader? ')
+if(a=='yes' or a == 'y'):
+    print 'great'
+else:
+    print 'you idiot'
+
 
