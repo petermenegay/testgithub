4904180c0bc1fc0a5a7dafe4e5579d80d09bb907
Peter Menegay <petermenegay@yahoo.com>
2024-09-18 10:26:27 -0400

    python3 compatible print stmts

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-print 'hi'
+print('hi')
 for i in range(10):
-    print i
-print 'bye'
-print 'something else'
+    print(i)
+print('bye')
+print('something else')
 #
-print 'now for some fun'
-a = raw_input('Are you smarter than a 3rd grader? ')
+print('now for some fun')
+a = input('Are you smarter than a 3rd grader? ')
 if(a=='yes' or a == 'y'):
-    print 'great'
+    print('great')
 else:
-    print 'you idiot'
+    print('you idiot')
+    
 
 
